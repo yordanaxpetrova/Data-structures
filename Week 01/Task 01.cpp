38e238477e0a6a4af9448b327f60618b7bec34d0
Yordana <yordanaxpetrova3@gmail.com>
2023-11-07 13:02:26 +0200

    Create Task 01.cpp
    The first missing number k between 1 and n. 
    Input: 1 to n numbers
    Output: k

---
+++ Week 01/Task 01.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int n;
+    cout << "Enter the value of n: ";
+    cin >> n;
+
+    bool numbers[100] = { false };  // Initialize an array to keep track of numbers
+
+    cout << "Enter numbers from 1 to " << n << " (0 to stop): ";
+    int num;
+    while (true) {
+        cin >> num;
+        if (num == 0) {
+            break;
+        }
+        if (num >= 1 && num <= n) {
+            numbers[num - 1] = true;  // Mark the entered number as found 
+        }
+        else {
+            cout << "Invalid input. Enter numbers from 1 to " << n << ": ";
+        }
+    }
+
+    int missingNumber = -1;
+    for (int i = 0; i < n; i++) {
+        if (!numbers[i]) {
+            missingNumber = i + 1;  // The first missing number
+            break;
+        }
+    }
+
+    if (missingNumber != -1) {
+        cout << "The first missing number k between 1 and " << n << " is: " << missingNumber << endl;
+    }
+    else {
+        cout << "No missing numbers found between 1 and " << n << endl;
+    }
+
+    return 0;
+}
